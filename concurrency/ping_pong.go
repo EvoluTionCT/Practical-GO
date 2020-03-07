c3ea1f75a94decf993d156777cabf5bae03f3088
Ratapong Ruksasuk <ratapong.ruksasuk29@gmail.com>
2020-03-07 17:03:23 +0700

    update code

--- concurrency/ping_pong.go
+++ concurrency/ping_pong.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+type Ball struct {
+	hits int
+}
+
+
+
 func main() {
-	
+	table := make(chan *Ball)
+
+	go play("Anime",table)
+	go play("Banana",table)
+
+	ball := new(Ball)
+	table <- ball
+	time.Sleep(3 * time.Second)
+	<-table //ส่งออกมาที่ Main
+}
+
+func play(name string,table chan *Ball) {
+	for {
+		ball := <- table
+		ball.hits++
+
+		fmt.Println(name, ball.hits)
+		time.Sleep(100 * time.Millisecond)
+
+		table <- ball
+	}
 }
