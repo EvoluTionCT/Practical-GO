c5abf035fdcfcef3bbdefac4dafdf7125cc677ac
Ratapong Ruksasuk <ratapong.ruksasuk29@gmail.com>
2020-03-07 13:30:01 +0700

    make api

--- server/server.go
+++ server/server.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/photos",func(w http.ResponseWriter,r *http.Request){
+		pht := []byte(`{"title":"Hello, It's me"}`)
+		w.WriteHeader(http.StatusOK)
+		w.Write(pht)
+	})
+	fmt.Println("Starting...")
 	
+	log.Fatal(http.ListenAndServe(":1234",nil))
 }
